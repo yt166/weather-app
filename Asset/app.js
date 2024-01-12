d565536873a06624eb2c9beba82ce792768dab7b
Yuwani Ranasinghe <yuwaniranasinghe@gmail.com>
2024-01-12 23:42:40 +0530

    connect to the API

--- Asset/app.js
+++ Asset/app.js
@@ -8,12 +8,14 @@ document.getElementById("btnSearch").addEventListener("click" ,()=>{
         method:'GET'
     };
 
-    fetch("http://api.weatherapi.com/v1/current.json?key=63e77c647e384bcba3280619241201&q=panadura",reop)
+    fetch(`http://api.weatherapi.com/v1/current.json?key=63e77c647e384bcba3280619241201&q=${input}`,reop)
     .then(responce => responce.json())
     .then(data =>{
         console.log(data);
 
-        document.getElementById("lbltemp").innerHTML=data["current"]["temp_c"];
+        document.getElementById("lbltemp").innerHTML=data["current"]["temp_c"]+"C";
+        document.getElementById("lblcountry").innerHTML=data["current"]["condition"]["text"];
+        document.getElementById("img").src=data["current"]["condition"]["icon"];
     })
     .then(error => console.log("error",error))
 })
