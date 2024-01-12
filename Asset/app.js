7b87a83cc811f875e1b850a6f0460a4bdde9b28b
Yuwani Ranasinghe <yuwaniranasinghe@gmail.com>
2024-01-12 22:55:34 +0530

    initial commit

---
+++ Asset/app.js
@@ -0,0 +1,19 @@
+console.log("Hi");
+
+document.getElementById("btnSearch").addEventListener("click" ,()=>{
+    let input = document.getElementById("gettxt").value;
+    //alert(input);
+
+    let reop ={
+        method:'GET'
+    };
+
+    fetch("http://api.weatherapi.com/v1/current.json?key=63e77c647e384bcba3280619241201&q=panadura",reop)
+    .then(responce => responce.json())
+    .then(data =>{
+        console.log(data);
+
+        document.getElementById("lbltemp").innerHTML=data["current"]["temp_c"];
+    })
+    .then(error => console.log("error",error))
+})
